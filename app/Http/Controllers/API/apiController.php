9d57d306b2200b486a4c333fd1426884a30fe9aa
nileshsutariya <nilesh.netdeveloper@gmail.com>
2024-09-24 15:26:26 +0530

    api

--- app/Http/Controllers/API/apiController.php
+++ app/Http/Controllers/API/apiController.php
@@ -1,10 +1,54 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class apiController extends Controller
 {
-    //
+    public function index(){
+        $category=Category::all();
+        $posts= Post::all();
+        return response()->json(compact('category','posts'));
+    }
+    public function category(){
+        $category=Category::all();
+        return response()->json($category);
+    }
+    public function post(){
+        $post=Post::all();
+        return response()->json($post);
+    }
+    public function login(Request $request){
+        // print_r("xdxfghjkl;");die;
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ])->validate();
+        $credentials = $request->only('email', 'password');
+        $email=$request->email;
+        $user =User::where('email',$email)->first(); 
+        if($user){
+            if ((int) $user->status == 1) {
+                if (Auth::attempt($credentials)) {
+                    return response()->json($user);
+                } else {
+                    Auth::logout();
+                    return response()->json('Wrong Password'); 
+                }
+            } else {
+                Auth::logout();
+                return response()->json( 'your account is not active'); 
+            }
+        } else {
+            Auth::logout();
+            return response()->json('you have to register first'); 
+        }
+    }
 }
